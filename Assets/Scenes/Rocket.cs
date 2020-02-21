c315c35db254c3325c2db88bc94aa115de3988de
Robert Lorch <lorchrob@gmail.com>
2020-02-21 16:49:05 -0600

    add stuff

---
+++ Assets/Scenes/Rocket.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    [SerializeField] float rcsThrust = 250f;
+    [SerializeField] float mainThrust = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
+        // can thrust while rotating
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        }
+    }
+
+    private void Rotate()
+    {
+        // take manual control of rotation
+        rigidBody.freezeRotation = true;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        // resume physics control of rotation
+        rigidBody.freezeRotation = false;
+    }
+}
